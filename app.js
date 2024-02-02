f0e2aef1152642d99935d0f24def618c24c6433c
BaoNQ <nqbao2712@gmail.com>
2024-02-02 09:49:48 +0700

    STUDY: [vue3-JS netninja] vue basics part 2 attribute binding and dynamic class

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const app = Vue.createApp({
     // template: '<h2>Template here!</h2>'
     data() {
         return {
+            url: "https://www.google.com",
             showBook: true,
             books: [
-                { title: 'The Final Empire', author: 'Brandon Sanderson', price: '80'},
-                { title: 'Name of the wind', author: 'Patrick Rothfuss', price: '180'},
-                { title: 'Harry Potter', author: 'J.K. Rowling', price: '280'}
+                { title: 'The Final Empire', author: 'Brandon Sanderson', price: '80', isFav: true},
+                { title: 'Name of the wind', author: 'Patrick Rothfuss', price: '180', isFav: false},
+                { title: 'Harry Potter', author: 'J.K. Rowling', price: '280', isFav: true}
             ],
         }
     },
     methods: {
         handleShowBook() {
             this.showBook = !this.showBook;
+        },
+        toggleFav(book) {
+            book.isFav = !book.isFav;
         },
     }
 })
