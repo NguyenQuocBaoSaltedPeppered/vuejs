008ebf1cc5c9af6b8631a1e36edcc3eb89cf883c
BaoNQ <nqbao2712@gmail.com>
2024-02-02 11:48:13 +0700

    STUDY: [vue3-JS netninja] vue-cli first look

--- vue-proj/src/App.vue
+++ vue-proj/src/App.vue
@@ -1,15 +1,33 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <h1>{{ title }}</h1>
+  <!-- <input type="text" ref="name">
+  <button @click="handleClick">click me!</button> -->
+  <Modal/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: "My first Vue App =)))",
+    }
+  },
+  methods: {
+    handleClick() {
+      // dùng template ref để lấy thông tin 1 element
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add('active');
+      this.$refs.name.focus();
+    }
   }
 }
 </script>
@@ -22,5 +40,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 2px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
