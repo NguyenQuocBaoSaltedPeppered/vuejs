fdfeab8565d1a52265cebbe390ee710642824759
BaoNQ <nqbao2712@gmail.com>
2024-02-19 15:51:37 +0700

    Feat: [STUDY] create Block and Result component

--- reaction-timer/src/App.vue
+++ reaction-timer/src/App.vue
@@ -1,26 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Racer reaction test</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+
+  <Block v-if="isPlaying" :delay="delay" @clicked="end"/>
+  <Result v-if="showResult" :score="score"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue';
+import Result from './components/Result.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {Block, Result},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false,
+    }
+  },
+  methods: {
+    start() {
+      // Mỗi lần chơi, Block sẽ xuất hiện trong khoảng từ 2 tới 6 giây.
+      this.delay = 2000 + Math.random() * 4000;
+      this.isPlaying = true;
+      this.showResult = false;
+      // console.log(`At [App.vue] Block appear after ${this.delay}ms`);
+    },
+    end(reactionTime) {
+      this.isPlaying = false;
+      this.score = reactionTime;
+      this.showResult = true;
+      // console.log(`At [App.vue] Stop timer at: ${this.score}ms`);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+  }
+  button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[display] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
 </style>
---
+++ reaction-timer/src/components/Result.vue
@@ -0,0 +1,35 @@
+<template>
+    <p>Reaction time: {{ score }}ms</p>
+    <p class="rank"> {{ rank }} </p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+
+        }
+    },
+    mounted() {
+        if (this.score < 250)
+        {
+            this.rank = 'At fast as flash huh! =)))';
+        } else if (this.score < 400)
+        {
+            this.rank = 'Not bad at all duh!';
+        } else {
+            this.rank = 'Snail pace ...';
+        }
+    }
+}
+</script>
+
+<style>
+    .rank{
+        font-size: 1.4em;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
