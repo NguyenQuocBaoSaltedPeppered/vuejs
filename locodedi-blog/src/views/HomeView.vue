93b2c6a9041c19996acf76f291dfadb0c97c6701
BaoNQ <nqbao2712@gmail.com>
2024-02-21 16:56:55 +0700

    Feat: [STUDY] composition API basics

--- locodedi-blog/src/views/HomeView.vue
+++ locodedi-blog/src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">Click me!</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  setup() {
+    console.log("setup")
+    let name = 'BaoNQ';
+    let age = 24;
+    const handleClick = () => {
+      console.log('You clicked me!');
+    };
+    return { name, age, handleClick};
+  },
 }
 </script>
