8e4f26ec7d094ef5c4c52a45bb8abac30d8d5475
BaoNQ <nqbao2712@gmail.com>
2024-02-22 09:45:32 +0700

    Feat: [STUDY] Computed values

--- locodedi-blog/src/views/HomeView.vue
+++ locodedi-blog/src/views/HomeView.vue
@@ -1,33 +1,26 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Ref</h2>
-    <p>{{ personOne.name }} - {{ personOne.age }}</p>
-    <button @click="updatePersonOne">Update person one</button>
-    <h2>Reactive</h2>
-    <p>{{ personTwo.name }} - {{ personTwo.age }}</p>
-    <button @click="updatePersonTwo">Update person two</button>
+    <input type="text" v-model="searchTerms">
+    <div v-for="name in matchingNames" :key="name" class="nameList">
+      <p>{{ name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from 'vue';
+import { computed, ref } from 'vue';
 
 export default {
   name: 'HomeView',
   setup() {
-    // Sự khác biệt là ko thể khởi tạo reactive với giá trị nguyên thuỷ.
-    // Nếu làm thì nó không còn đạt được hiệu quả reactive nữa.
-    // Do đó reactive dù nhìn tiện hơn, ref lại có vẻ ổn và dễ dùng hơn.
-    const personOne = ref({ name: 'BaoNQ', age: 24});
-    const personTwo = reactive({ name: 'ChienHV', age: 23});
-    const updatePersonOne = () => {
-      personOne.value.age += 10;
-    };
-    const updatePersonTwo = () => {
-      personTwo.age += 10;
-    };
-    return { personOne, personTwo, updatePersonOne, updatePersonTwo};
+    const names = ref(['reyna', 'neon', 'jett', 'sage', 'omen', 'killjoy']);
+    const searchTerms = ref('');
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(searchTerms.value));
+    })
+    return { names, searchTerms, matchingNames };
   },
 }
 </script>
