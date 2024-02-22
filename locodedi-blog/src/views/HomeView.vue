511ad9abe3d98619cc476418e808d4fe9a8388b8
BaoNQ <nqbao2712@gmail.com>
2024-02-22 09:27:15 +0700

    Feat: [STUDY] ref vs reactive

--- locodedi-blog/src/views/HomeView.vue
+++ locodedi-blog/src/views/HomeView.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click me!</button>
-    <button @click="age+=10">Add 10 to age</button>
-    <input type="text" v-model="name">
+    <h2>Ref</h2>
+    <p>{{ personOne.name }} - {{ personOne.age }}</p>
+    <button @click="updatePersonOne">Update person one</button>
+    <h2>Reactive</h2>
+    <p>{{ personTwo.name }} - {{ personTwo.age }}</p>
+    <button @click="updatePersonTwo">Update person two</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 export default {
   name: 'HomeView',
   setup() {
-    const name = ref('BaoNQ');
-    const age = ref(24);
-    const handleClick = () => {
-      name.value = 'BaoNQ pro max';
-      age.value++;
+    // Sự khác biệt là ko thể khởi tạo reactive với giá trị nguyên thuỷ.
+    // Nếu làm thì nó không còn đạt được hiệu quả reactive nữa.
+    // Do đó reactive dù nhìn tiện hơn, ref lại có vẻ ổn và dễ dùng hơn.
+    const personOne = ref({ name: 'BaoNQ', age: 24});
+    const personTwo = reactive({ name: 'ChienHV', age: 23});
+    const updatePersonOne = () => {
+      personOne.value.age += 10;
     };
-    return { name, age, handleClick};
+    const updatePersonTwo = () => {
+      personTwo.age += 10;
+    };
+    return { personOne, personTwo, updatePersonOne, updatePersonTwo};
   },
 }
 </script>
