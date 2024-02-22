6375915969953b61107e2fc0c487824f6b215e01
BaoNQ <nqbao2712@gmail.com>
2024-02-22 09:58:01 +0700

    Feat: [STUDY] watch and watchEffect

--- locodedi-blog/src/views/HomeView.vue
+++ locodedi-blog/src/views/HomeView.vue
@@ -5,11 +5,12 @@
     <div v-for="name in matchingNames" :key="name" class="nameList">
       <p>{{ name }}</p>
     </div>
+    <button @click="stopWatching">Stop watching</button>
   </div>
 </template>
 
 <script>
-import { computed, ref } from 'vue';
+import { computed, ref, watch, watchEffect } from 'vue';
 
 export default {
   name: 'HomeView',
@@ -17,10 +18,27 @@ export default {
     const names = ref(['reyna', 'neon', 'jett', 'sage', 'omen', 'killjoy']);
     const searchTerms = ref('');
 
+    // Không chạy lúc đầu, chỉ chạy khi searchTerms thay đổi
+    const stopWatch = watch(searchTerms, () => {
+      console.log('Watch funciton');
+    })
+
+    // Chạy lần đầu tiên và chạy lại khi giá trị bên trong hàm bị thay đổi
+    // Ở đây là mỗi lần gõ, searchTerms thay đổi giá trị và vì nó đang nằm
+    // trong hàm này, watchEffect sẽ biết được.
+    const stopWatchEffect = watchEffect(() => {
+      console.log('WatchEffect function ', searchTerms.value)
+    })
+
+    const stopWatching = () => {
+      stopWatch();
+      stopWatchEffect();
+    }
+
     const matchingNames = computed(() => {
       return names.value.filter((name) => name.includes(searchTerms.value));
     })
-    return { names, searchTerms, matchingNames };
+    return { searchTerms, matchingNames, stopWatch, stopWatchEffect, stopWatching };
   },
 }
 </script>
